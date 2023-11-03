31a49535880693be5de01df845abc0e26f74f920
aram <dev.aram0717@gmail.com>
2023-11-03 17:50:55 +0900

    valigator 추가

---
+++ data/auth.js
@@ -0,0 +1,10 @@
+let users = [
+    {
+        id: '1',
+        username: 'apple',
+        password: '$2b$10$6NVVL4gEtPh684Ncn2sCRe/LPe0u4kRkhBYSoiLx4bTGW5gwQ58Dy',
+        name:'김사과',
+        email:'apple@apple.com',
+        url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrYEhHx-OXQF1NqVRXPL8R50ggKje3hQTvIA&usqp=CAU'
+    },
+]
---
+++ router/auth.js
@@ -0,0 +1,12 @@
+import express  from "express";
+
+/*
+    회원가입 / post
+    router.post('/signup,...)
+
+    로그인 / post
+    router.post('/login',...)
+
+    JWT 확인 / get
+    router.get('/me',...)
+*/
