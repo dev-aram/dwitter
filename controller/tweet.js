d8bd95c34be3b19dcbc6fdadc0f9baa4c4d06c81
aram <dev.aram0717@gmail.com>
2023-11-07 08:36:48 +0900

    주석처리

--- controller/tweet.js
+++ controller/tweet.js
@@ -1,10 +1,12 @@
 import * as tweetRepository from '../data/tweets.js';
 
+// 데이터를 여기에서 로직처리
+
 export async function getTweets(req,res){ 
     const username = req.query.username
     const data = await (username
-        ? tweetRepository.getAllByUsername(username)
-        : tweetRepository.getAll()
+        ? tweetRepository.getAllByUsername(username) // username 있으면 해당 유저의 글만 추출
+        : tweetRepository.getAll() // username없으면 전체게시물 추출
         )
         return res.status(200).json(data)
 }
@@ -12,7 +14,7 @@ export async function getTweets(req,res){
 // getTweet
 export async function getTweet(req,res,next) {
     const id = req.params.id
-    const tweet =await tweetRepository.getById(id)
+    const tweet =await tweetRepository.getById(id) //getByID는 특정게시물 아이디를 가진애 추출
     if(tweet){
         return res.status(200).json(tweet)
     }else{
@@ -23,7 +25,7 @@ export async function getTweet(req,res,next) {
 // createTweet
 export async function createTweet(req,res,next){
     const {text} = req.body
-    const tweet = await tweetRepository.create(text, req.userId)
+    const tweet = await tweetRepository.create(text, req.userId) // 만들 텍스트와 로그인한 유저(토큰)의 아이디 전달해서 무조건 자신의 게시물을 쓰게끔
     res.status(201).json(tweet)
 }
 
@@ -31,32 +33,32 @@ export async function createTweet(req,res,next){
 export async function updateTweet(req,res,next){
     const id = req.params.id;
     const text = req.body.text;
-    const tweet = await tweetRepository.getById(id)
+    const tweet = await tweetRepository.getById(id) //바꾸고싶은 게시글아이디로 검색해서 게시글 불러옴
 
     if(!tweet) {
         return res.status(404).json({message:`Tweet id(${id}) not found`})
 
     }
-    if(req.userId !== tweet.userId){
-        return res.status(404).json({message:`작성자가 아닙니다.`})
+    if(req.userId !== tweet.userId){ // 로그인한 유저(req.userid)와 게시물작성자아이디 일치하는지 확인
+        return res.status(404).json({message:`작성자가 아닙니다.`}) //일치안할시
     }
-    const update = await tweetRepository.update(id,text)
+    const update = await tweetRepository.update(id,text) //일치할시
     return res.status(200).json(update)
 }
 
 //deleteTweet
 export async function deleteTweet(req,res,next){
     const id =req.params.id
-    const tweet = await tweetRepository.getById(id)
+    const tweet = await tweetRepository.getById(id) //삭제하고 싶은 게시물을 가지고옴
 
     if(!tweet) {
-        return res.status(404).json({message:`Tweet id(${id}) not found`})
+        return res.status(404).json({message:`Tweet id(${id}) not found`}) //게시물 없을때
 
     }
-    if(req.userId !== tweet.userId){
+    if(req.userId !== tweet.userId){ //로그인한 유저와 게시글작성자 같은지 확인
         return res.status(404).json({message:`작성자가 아닙니다.`})
     }
-    await tweetRepository.remove(id)
+    await tweetRepository.remove(id) // 다 조건에 맞을경우 삭제!
     return res.sendStatus(204)
 
 }
--- data/tweets.js
+++ data/tweets.js
@@ -1,5 +1,5 @@
 import * as userRepository from '../data/auth.js';
-let tweets = [
+let tweets = [// 배열안에 객체 생성
     {
         id : '1',
         text : '안녕하세요',
@@ -14,37 +14,40 @@ let tweets = [
     }
 ];
 
-export async function getAll(){
-    return Promise.all(
-        tweets.map( async(tweet) =>{
-            const {username, name, url} = await userRepository.findById(tweet.userId);
-            return {...tweet, username, name, url}
+// Promise.all은 모든 Promise의 결과가 필요하고 순서가 중요하지 않을 때 유용
+//await는 결과를 개별적으로 처리하며, 순서가 중요하고 이전 작업의 결과를 이용하여 다음 작업을 수행해야 할 때 유용
+export async function getAll(){ // 모든 게시물별로 auth에 있는 유저정보를 추가해 모든 게시물을 반환
+    return Promise.all( // Promise.all -> 비동기 작업을 병렬로 처리하고 모든 작업이 완료될때까지 기다림 
+        tweets.map( async(tweet) =>{ // 배열을 순회하면서 각 요소에 대한 변환 또는 처리를 수행하는 배열 메서드
+            const {username, name, url} = await userRepository.findById(tweet.userId); // 게시물의 작성자아이디와 같은 유저의 정보를 가져옴
+            return {...tweet, username, name, url} // 게시글과 유저정보를 합
         })
     );
-}
+} //데이터를 가져오는 함수 생성(가져오는 동안 에러나면 안되니까 비동기로 처리)
 
-export async function getAllByUsername(username){
-    return getAll().then((tweets) => tweets.filter((tweet) => tweet.username === username));
-}
+export async function getAllByUsername(username){ // 특정 유저의 게시글 전부 추출(근데이제url에 입력한애로)
+    // getAll()되면 then으로 감, 안되면 catch로 감 여기서 catch는 생략
+    return getAll().then((tweets) => tweets.filter((tweet) => tweet.username === username)); //filter는 여러개반환가능,find는 하나만 반환
+} // then을 써서 성공하면 실행할 코드를 작성
 
-export async function getById(id) {
-    const found = tweets.find((tweet) => tweet.id === id);
+export async function getById(id) {// 특정 게시물 검색
+    const found = tweets.find((tweet) => tweet.id === id); // 게시물리스트에서 게시물아이디로 검색
     if(!found){
         return nill;
     }
-    const {username, name, url} = await userRepository.findById(found.userId);
+    const {username, name, url} = await userRepository.findById(found.userId); //원하는 게시물 작성자의 정보 추출
     return {...found, username, name, url}
 }
 
 export async function create(text,userId) {
     const tweet = {
         id:'10',
-        text,
+        text, //자바는 키와 값의 이름이 같다면 생략가능 지금 text:text의 생략이 text
         createdAt: Date.now().toString(),
         userId
     }
-    tweets = [tweet, ... tweets]
-    return getById(tweet.id)
+    tweets = [tweet, ... tweets] // 수정될 것을 대비해서 트윗을 트윗스에 저장해줌 그러면 이제 서로 다른 메모리값을 가짐
+    return getById(tweet.id) // 게시물 아이디로 검색한 결과(게시물)리턴
 }
 
 export async function update(id,text){
@@ -52,7 +55,7 @@ export async function update(id,text){
     if(tweet){
         tweet.text = text
     }
-    return getById(tweet.id)
+    return getById(tweet.id) // 게시물 아이디로 검색한 결과(게시물)리턴
 }
 
 export async function remove(id){
